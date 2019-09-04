0235538b253575d1a55e5edc695a74c36cd880ff
pilgyumin <pilgyu12@naver.com>
2019-09-04 15:43:08 +0900

    Revert "2019-09-01 라즈베리파이 -> 아두이노 기기 제어부 통신 개발"
    This reverts commit 5251bd422cf4273972775066b2f7855367062027.

--- Webserver/server/routes/index.js
+++ Webserver/server/routes/index.js
@@ -1,38 +1,11 @@
-const express = require('express');
-const http = require('http');
-const router = express.Router();
-
-let aircleaner = {
-    hostname: '192.168.0.10',
-    port: '3000',
-    path : '?'
-};
-
-let humidifier = {
-    hostname: 'localhost',
-    port: '3000',
-    path : '?'
-};
-let airconditioner = {
-    hostname: '192.168.0.10',
-    port: '3000',
-    path : '?'
-};
-
+var express = require('express');
+var router = express.Router();
 
 router.get('/', (req, res, next) => {
     //temp
     if(req.query.tempOuter){
-        let tOut = req.query.tempOuter.split(".");
-        console.log("tempOuter : " + tOut[0]);
-        if(tOut[0] > 30){
-            console.log("Aircon On!");
-            airconditioner.path += tOut[0];
-            console.log(airconditioner.path);
-            http.request(airconditioner).end();
-        }
+    	console.log("tempOuter : " + req.query.tempOuter);    
     }
-
     if(req.query.tempInner){
         console.log("tempInner : " + req.query.tempInner);   
     }
@@ -41,24 +14,14 @@ router.get('/', (req, res, next) => {
     if(req.query.humidOuter){
     	console.log("humidOuter : " + req.query.humidOuter);    
     }
-
     if(req.query.humidInner){
     	console.log("humidInner : " + req.query.humidInner);    
     }
 
     //pm10
     if(req.query.pm10Outer){
-        let pOut = req.query.pm10Outer.split(".");
-        console.log("pm10Outer : " + pOut[0]);
-        if(pOut[0] > 30){
-            console.log("Aircleaner On!");
-            aircleaner.path += '0x48B7C837';
-            console.log(aircleaner.path);
-            http.request(aircleaner).end();
-            aircleaner.path = '?';
-        }
+    	console.log("pm10Outer : " + req.query.pm10Outer);    
     }
-
     if(req.query.pm10Inner){
     	console.log("pm10Inner : " + req.query.pm10Inner);   
     }
@@ -67,7 +30,6 @@ router.get('/', (req, res, next) => {
     if(req.query.pm25Outer){
     	console.log("pm25Outer : " + req.query.pm25Outer);    
     }
-
     if(req.query.pm25Inner){
     	console.log("pm25Inner : " + req.query.pm25Inner);   
     }
@@ -76,12 +38,9 @@ router.get('/', (req, res, next) => {
     if(req.query.vocOuter){
     	console.log("vocOuter : " + req.query.vocOuter);    
     }
-
     if(req.query.vocInner){
     	console.log("vocInner : " + req.query.vocInner);   	
     }
-
-    res.json(JSON.stringify(aircleaner));
 });
 
 module.exports = router;
