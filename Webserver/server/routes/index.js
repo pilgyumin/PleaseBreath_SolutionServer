e6c95b2f200c0ca3ca80b56787b94de0596232c6
pilgyumin <pilgyu12@naver.com>
2019-09-04 15:43:02 +0900

    Revert "2019-09-04 공기청정기 제어"
    This reverts commit e8fbd1d52f31790934fe80cfa9ca72f08248875e.

--- Webserver/server/routes/index.js
+++ Webserver/server/routes/index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const router = express.Router();
 
 let aircleaner = {
-    hostname: '192.168.0.7',
+    hostname: '192.168.0.10',
     port: '3000',
     path : '?'
 };
@@ -14,36 +14,36 @@ let humidifier = {
     path : '?'
 };
 let airconditioner = {
-    hostname: '192.168.0.7',
+    hostname: '192.168.0.10',
     port: '3000',
     path : '?'
 };
+
 
 router.get('/', (req, res, next) => {
     //temp
     if(req.query.tempOuter){
         let tOut = req.query.tempOuter.split(".");
         console.log("tempOuter : " + tOut[0]);
-        if(tOut[0] > 3){
+        if(tOut[0] > 30){
             console.log("Aircon On!");
-            airconditioner.path += '0x48B7C837';
+            airconditioner.path += tOut[0];
             console.log(airconditioner.path);
             http.request(airconditioner).end();
-            airconditioner.path = '?';
         }
     }
 
     if(req.query.tempInner){
-        console.log("tempInner : " + req.query.tempInner);
+        console.log("tempInner : " + req.query.tempInner);   
     }
 
     //humid
     if(req.query.humidOuter){
-        console.log("humidOuter : " + req.query.humidOuter);
+    	console.log("humidOuter : " + req.query.humidOuter);    
     }
 
     if(req.query.humidInner){
-        console.log("humidInner : " + req.query.humidInner);
+    	console.log("humidInner : " + req.query.humidInner);    
     }
 
     //pm10
@@ -53,38 +53,35 @@ router.get('/', (req, res, next) => {
         if(pOut[0] > 30){
             console.log("Aircleaner On!");
             aircleaner.path += '0x48B7C837';
-            if(pOut[0] > 80){
-                console.log("Aircleaner On!");
-                aircleaner.path += ' 0x48B748B7';
-            }
+            console.log(aircleaner.path);
             http.request(aircleaner).end();
             aircleaner.path = '?';
         }
     }
 
     if(req.query.pm10Inner){
-        console.log("pm10Inner : " + req.query.pm10Inner);
+    	console.log("pm10Inner : " + req.query.pm10Inner);   
     }
 
     //pm2.5
     if(req.query.pm25Outer){
-        console.log("pm25Outer : " + req.query.pm25Outer);
+    	console.log("pm25Outer : " + req.query.pm25Outer);    
     }
 
     if(req.query.pm25Inner){
-        console.log("pm25Inner : " + req.query.pm25Inner);
+    	console.log("pm25Inner : " + req.query.pm25Inner);   
     }
 
     //voc
     if(req.query.vocOuter){
-        console.log("vocOuter : " + req.query.vocOuter);
+    	console.log("vocOuter : " + req.query.vocOuter);    
     }
 
     if(req.query.vocInner){
-        console.log("vocInner : " + req.query.vocInner);
+    	console.log("vocInner : " + req.query.vocInner);   	
     }
 
-    res.status(200);
+    res.json(JSON.stringify(aircleaner));
 });
 
 module.exports = router;
