c90b344236fa25b2b03d3d41d53fff8ca99df987
pilgyumin <pilgyu12@naver.com>
2019-09-04 19:28:41 +0900

    라즈베리파이 -> 웹 서버 데이터 전송 완료

--- Webserver/server/routes/index.js
+++ Webserver/server/routes/index.js
@@ -19,72 +19,147 @@ let airconditioner = {
     path : '?'
 };
 
-router.get('/', (req, res, next) => {
+let webserver = {
+    hostname: 'localhost',
+    port: '80',
+    path : '/insertdb?'
+};
+
+router.get('', (req, res, next) => {
+
     //temp
     if(req.query.tempOuter){
         let tOut = req.query.tempOuter.split(".");
+        webserver.path += 'tempOuter='+tOut[0];
         console.log("tempOuter : " + tOut[0]);
         if(tOut[0] > 3){
             console.log("Aircon On!");
             airconditioner.path += '0x48B7C837';
             console.log(airconditioner.path);
-            http.request(airconditioner).end();
-            airconditioner.path = '?';
+           // http.request(airconditioner).end();
         }
     }
 
     if(req.query.tempInner){
-        console.log("tempInner : " + req.query.tempInner);
+        let tIn = req.query.tempInner.split(".");
+        webserver.path += 'tempInner='+tIn[0];
+        console.log("tempInner : " + tIn[0]);
+        if(tIn[0] > 3){
+            console.log("Aircon On!");
+            airconditioner.path += '0x48B7C837';
+            console.log(airconditioner.path);
+            //http.request(airconditioner).end();
+        }
     }
 
     //humid
     if(req.query.humidOuter){
-        console.log("humidOuter : " + req.query.humidOuter);
+        let hOut = req.query.humidOuter.split(".");
+        webserver.path += 'humidOuter='+hOut[0];
+        console.log("humidOuter : " + hOut[0]);
+        if(hOut[0] > 3){
+            console.log("humidifier On!");
+            humidifier.path += '0x48B7C837';
+            console.log(humidifier.path);
+           // http.request(humidifier).end();
+        }
     }
 
     if(req.query.humidInner){
-        console.log("humidInner : " + req.query.humidInner);
+        let hIn = req.query.humidInner.split(".");
+        webserver.path += 'humidInner='+hIn[0];
+        console.log("humidInner : " + hIn[0]);
+        if(hIn[0] > 3){
+            console.log("humidifier On!");
+            airconditioner.path += '0x48B7C837';
+            console.log(airconditioner.path);
+          //  http.request(airconditioner).end();
+        }
     }
 
     //pm10
     if(req.query.pm10Outer){
-        let pOut = req.query.pm10Outer.split(".");
-        console.log("pm10Outer : " + pOut[0]);
-        if(pOut[0] > 30){
-            console.log("Aircleaner On!");
-            aircleaner.path += '0x48B7C837';
-            if(pOut[0] > 80){
-                console.log("Aircleaner On!");
-                aircleaner.path += ' 0x48B748B7';
-            }
-            http.request(aircleaner).end();
-            aircleaner.path = '?';
+        let p10Out = req.query.pm10Outer.split(".");
+        webserver.path += 'pm10Outer='+p10Out[0];
+        console.log("pm10Outer : " + p10Out[0]);
+        if(p10Out[0] > 3){
+            console.log("Aircon On!");
+            airconditioner.path += '0x48B7C837';
+            console.log(airconditioner.path);
+          //  http.request(airconditioner).end();
         }
     }
 
     if(req.query.pm10Inner){
-        console.log("pm10Inner : " + req.query.pm10Inner);
+        let p10In = req.query.pm10Inner.split(".");
+        webserver.path += 'pm10Inner='+p10In[0];
+        console.log("pm10Inner : " + p10In[0]);
+        if(p10In[0] > 3){
+            console.log("Aircon On!");
+            airconditioner.path += '0x48B7C837';
+            console.log(airconditioner.path);
+          //  http.request(airconditioner).end();
+        }
     }
 
     //pm2.5
     if(req.query.pm25Outer){
-        console.log("pm25Outer : " + req.query.pm25Outer);
+        let p25Out = req.query.pm25Outer.split(".");
+        webserver.path += 'pm25Outer='+p25Out[0];
+        console.log("pm25Outer : " + p25Out[0]);
+        if(p25Out[0] > 3){
+            console.log("Aircon On!");
+            airconditioner.path += '0x48B7C837';
+            console.log(airconditioner.path);
+         //   http.request(airconditioner).end();
+        }
     }
 
     if(req.query.pm25Inner){
-        console.log("pm25Inner : " + req.query.pm25Inner);
+        let p25In = req.query.pm25Inner.split(".");
+        webserver.path += 'pm25Inner='+p25In[0];
+        console.log("pm25Inner : " + p25In[0]);
+        if(p25In[0] > 3){
+            console.log("Aircon On!");
+            airconditioner.path += '0x48B7C837';
+            console.log(airconditioner.path);
+         //   http.request(airconditioner).end();
+        }
     }
 
     //voc
     if(req.query.vocOuter){
-        console.log("vocOuter : " + req.query.vocOuter);
+        let vOut = req.query.vocOuter.split(".");
+        webserver.path += 'vocOuter='+vOut[0];
+        console.log("vocOuter : " + vOut[0]);
+        if(vOut[0] > 3){
+            console.log("Aircon On!");
+            airconditioner.path += '0x48B7C837';
+            console.log(airconditioner.path);
+         //   http.request(airconditioner).end();
+        }
     }
 
     if(req.query.vocInner){
-        console.log("vocInner : " + req.query.vocInner);
+        let vIn = req.query.vocInner.split(".");
+        webserver.path += 'vocInner='+vIn[0];
+        console.log("vocInner : " + vIn[0]);
+        if(vIn[0] > 3){
+            console.log("Aircon On!");
+            airconditioner.path += '0x48B7C837';
+            console.log(airconditioner.path);
+         //   http.request(airconditioner).end();
+
+        }
     }
 
-    res.status(200);
+    http.request(webserver).end();
+    webserver.path = '/insertdb?';
+    airconditioner.path = '?';
+    humidifier.path = '?';
+    aircleaner.path = '?';
+
+    res.json(JSON.stringify(webserver));
 });
 
 module.exports = router;
