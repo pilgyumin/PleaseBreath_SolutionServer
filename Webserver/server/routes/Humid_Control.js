329199e39b11ef3d21ccf00136ec39b337958d07
조현학 <zhh102000@gmail.com>
2019-10-18 20:54:49 +0900

    [RUN-22][가습기 router 변경 speed_up, speed_down, 최대 속도 8 최저 속도 1][노인 모드]

--- Webserver/server/routes/Humid_Control.js
+++ Webserver/server/routes/Humid_Control.js
@@ -9,7 +9,6 @@ const Humid_Control_Url = require('../url_Model/humidifier_Url');
 router.get('/power', (req, res, next) => {
 	humidifier.power=1-humidifier.power;
 	if(humidifier.power==1){
-		humidifier.humid=40;
 		humidifier.speed=1;
 	}
 	console.log('Humid_PowerOn');
@@ -20,6 +19,30 @@ router.get('/power', (req, res, next) => {
 	Humid_Control_Url.path = '?';
 	res.json(JSON.stringify({}));
 });
+
+
+router.get('/speed_up', (req, res, next) => {
+	if(humidifier.speed != 8){
+		humidifier.speed += 1;
+	console.log('Humid_speed_up');
+	Humid_Control_Url.path += humidifier.control.speed_up;
+	http.request(Humid_Control_Url).end();
+	Humid_Control_Url.path = '?';
+	res.json(JSON.stringify({}));
+	}
+});
+
+router.get('/speed_down', (req, res, next) => {
+	if(humidifier.speed != 1){
+		humidifier.speed -= 1;
+	console.log('Humid_speed_down');
+	Humid_Control_Url.path += humidifier.control.speed_down;
+	http.request(Humid_Control_Url).end();
+	Humid_Control_Url.path = '?';
+	res.json(JSON.stringify({}));
+	}
+});
+
 
 router.get('/mist', (req, res, next) =>{
 	if(humidifier.humid==90){
@@ -43,5 +66,7 @@ router.get('/humidity', (req, res, next) =>{
 	Humid_Control_Url.path = '?';
 	res.json(JSON.stringify({}));
 });
+
+
 
 module.exports = router;
