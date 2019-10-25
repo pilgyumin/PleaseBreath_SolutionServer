c7fdc2b82a37826d9c3c16c6a3ebd1c6e4207a0f
조현학 <zhh102000@gmail.com>
2019-10-25 16:53:57 +0900

    [run 104][서버로 받은 speedup, tempup, tempdown 데이터 처리][라즈베리파이 에어컨 제어]

--- Webserver/server/routes/Airconditioner_Control.js
+++ Webserver/server/routes/Airconditioner_Control.js
@@ -4,15 +4,27 @@ const router = express.Router();
 
 const airconditioner_Url = require('../url_Model/airconditioner_Url');
 const airconditioner = require('../model/airconditioner');
+const Airconditioner_Control = require('../machine_control/airconditioner_control');
 
 router.get('/power', (req, res, next) => {
-    airconditioner.power=1-airconditioner.power;
+    Airconditioner_Control.Airconditioner_Power();
     console.log('Airconditioner Power');
-    airconditioner_Url.path += airconditioner.control.power;
-    console.log(airconditioner.control.power);
-    http.request(airconditioner_Url).end();
-    airconditioner_Url.path = '?';
-    res.json(JSON.stringify({}));
+    Airconditioner_Control.Airconditioner_Send_command();
 });
+
+
+router.get('/speedup', (req, res, next) => {
+    Airconditioner_Control.Airconditioner_Speed();
+    Airconditioner_Control.Airconditioner_Send_command();
+});
+
+router.get('/tempup', (req, res, next) => {
+    Airconditioner_Control.Airconditioner_Temp(0,2);
+});
+
+router.get('/tempdown', (req, res, next) => {
+    Airconditioner_Control.Airconditioner_Temp(0,1);
+});
+
 
 module.exports = router;
