5aa0c596e28750c41ce936805c48c6adda12e751
pilgyumin <pilgyu12@naver.com>
2019-11-19 20:45:08 +0900

    [RUN-128][* 추가 기능 [메인 페이지] 모듈 개발 및 UI 결합]

--- Webserver/server/routes/get_solution_status.js
+++ Webserver/server/routes/get_solution_status.js
@@ -4,9 +4,32 @@ const router = express.Router();
 
 const solution_status = require('../model/solution_status');
 const inner_status = require('../model/status_Inner');
+const outer_status = require('../model/status_Outer');
 const airconditioner_status = require('../model/airconditioner');
+router.post('/get',(req,res,next)=>{
+    let data ={
+        power : 0,
+        temp : 0,
+        humid : 0
+    }
 
-router.get('/:mode',(req,res,next)=>{
+    data.power = solution_status.mode;
+    data.temp = inner_status.temp_Inner;
+    data.humid = inner_status.humid_Inner;
+
+    console.log(JSON.stringify(data));
+
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header('Content-Type', 'application/json');
+    res.header('Accept' , 'application/json');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Expires", 0);
+    res.json(JSON.stringify(data));
+
+});
+
+router.post('/:mode',(req,res,next)=>{
     if(req.params.mode === 'get'){
         let data ={
             power : 0,
@@ -16,22 +39,33 @@ router.get('/:mode',(req,res,next)=>{
 
         data.power = solution_status.mode;
         data.temp = inner_status.temp_Inner;
-        data.temp = inner_status.humid_Inner;
+        data.humid = inner_status.humid_Inner;
 
-        airconditioner_status.detail_mode = 1;
+        console.log(JSON.stringify(data));
 
-        console.log(solution_status.mode);
         res.header("Cache-Control", "no-cache, no-store, must-revalidate");
         res.header("Pragma", "no-cache");
         res.header('Content-Type', 'application/json');
         res.header('Accept' , 'application/json');
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Expires", 0);
-        console.log(JSON.stringify(data));
+
         res.json(JSON.stringify(data));
     }
     else{
+        airconditioner_status.detail_mode = 1;
 
+        if(outer_status.temp_Outer < 18){
+            // 난방 모드로 변경
+            airconditioner_status.warm.temp = req.body.temp;
+        }
+        else{
+            // 냉방 모드로 변경
+            airconditioner_status.cold.temp = req.body.temp;
+        }
+        airconditioner_status.humid = req.body.humid;
+        console.log(JSON.stringify(req.body));
+        res.json({});
     }
 
 });
